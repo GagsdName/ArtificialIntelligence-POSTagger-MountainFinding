39b271fa20b592529db1247572e6f9e6ab20c73b
Manan Pancholi <mpanchol@iu.edu>
2016-11-01 14:27:18 -0400

    Added Smoothing to startProbability and tagConditionalProbability

--- part1/TrainStatistics.py
+++ part1/TrainStatistics.py
@@ -68,10 +68,16 @@ class TrainStatistics:
 # Returns the probability of the given tag to occur in the beginning of a sentence
 # n = Total number of sentences in the training data
     def getStartProbability(self, tag, n):
-        return self.sentenceStartCount[tag]/n
-# Returns the prior probability of tag1 given tag2: P(tag1/tag2)
+        return self.sentenceStartCount[tag]/n if tag in self.sentenceStartCount else 0.5/n
+# Returns the prior probability of tag1 given tag2: P(tag1/tag2) with Smoothing
     def getTagConditionalProbability(self, tag1, tag2):
-        return self.bigramCount[tag2+'#'+tag1]/self.individualCount[tag2]
-# Returns the probability of a word (W) given a tag: P(W/S)
+        if self.individualCount[tag2]==0:
+            return 0
+        temp = tag2+'#'+tag1
+        return self.bigramCount[temp]/self.individualCount[tag2] if temp in self.bigramCount else (0.5/self.individualCount[tag2])
+# Returns the probability of a word (W) given a tag: P(W/S) without Smoothing
     def getWordConditionalProbability(self, word, tag):
-        return self.wordToTagCount[word+'#'+tag]/self.individualCount[tag]
+        if self.individualCount[tag]==0:
+            return 0
+        temp = word+'#'+tag
+        return self.wordToTagCount[temp]/self.individualCount[tag]
